cc31db224db450477423f76d4504aa216eea62b6
Your Name <you@example.com>
2023-07-25 08:27:26 -0700

    First tests passing

---
+++ lib/remote_control_car.ex
@@ -0,0 +1,19 @@
+defmodule RemoteControlCar do
+  defstruct [:nickname, battery_percentage: 100, distance_driven_in_meters: 0]
+
+  def new(nickname \\ "none") do
+    %RemoteControlCar{nickname: nickname}
+  end
+
+  def display_distance(remote_car) do
+    # Please implement the display_distance/1 function
+  end
+
+  def display_battery(remote_car) do
+    # Please implement the display_battery/1 function
+  end
+
+  def drive(remote_car) do
+    # Please implement the drive/1 function
+  end
+end
